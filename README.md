3169bff9473df5063012fb8f34b988eac0dd209f
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-04-09 17:21:05 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # exo-async-0
+
+## my-wget.js
+
+my-wget.js which takes as parameter on the command line an url, which downloads its contents, and writes it in an index.html file on your disk.
+
+Play the program on your terminal : `node my-wget.js YOURURL`
--- my-wget.js
+++ my-wget.js
@@ -3,7 +3,7 @@ const fsPromises = require('fs/promises')
 const chalk = require('chalk');
 
 
-// Function vérifie si url est valide
+// Function checks if input is URL valid
 function isValidURL(input) {
   var res = input.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
   return (res !== null)
@@ -14,7 +14,7 @@ if (process.argv.length != 3) {
   process.exit(1)
 }
 
-// vérifie si url est valide
+// check if url is valid
 if (!isValidURL(process.argv[2])) {
   console.log(`Error: ${process.argv[2]} is not a url.`)
   process.exit(1)
@@ -32,7 +32,7 @@ const main = async () => {
     const stats = await fsPromises.stat(fileOutput)
 
     const fileSize = (`${stats.size/1000} ko`) 
-    console.log(`le contenu du site ${chalk.blue(process.argv[2])} a bien été copier dans le fichier ${chalk.blue(fileOutput)} et le fichier pèse environ ${chalk.blue(fileSize)}`)
+    console.log(`the content of the site ${chalk.blue(process.argv[2])} has been copied into the ${chalk.blue(fileOutput)} file and the file weighs approximately ${chalk.blue(fileSize)}`)
   } catch (e) {
     console.log(e.message)
   }
